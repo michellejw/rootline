e1fa3fa57aeb6826fbbef368e8e74d2da0c79983
Michelle Weirathmueller <michellejw@gmail.com>
2026-06-15 23:31:15 -0400

    Initial Commit

---
+++ rootline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rootline
+//
+//  Created by Michelle Weirathmueller on 6/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rootline/rootlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  rootlineApp.swift
+//  rootline
+//
+//  Created by Michelle Weirathmueller on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct rootlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
